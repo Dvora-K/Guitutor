f1031bd777d684c2cdd87c54d8914683e8601a5e
Dvora-K <150067810+Dvora-K@users.noreply.github.com>
2024-06-05 13:20:39 +0300

    fix build_chord_classifcation_model_rfc.py

--- Guitutor - server/build_chord_classifcation_model_rfc.py
+++ Guitutor - server/build_chord_classifcation_model_rfc.py
@@ -1,17 +1,25 @@
+import os
+import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import confusion_matrix, accuracy_score
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-import pandas as pd
-import os
-import joblib
 
 from recognize_chord_name import find_harmonics
 
+
 def build_model(path):
-    # Build model
+    # build model
     data = []
-    max_harm_length = 0  # Track max harmonic length for naming columns
+    # 278, 55.0
+    max_harm_length = 0  # i will keep track of max harmonic length for naming columns
 
     for dirname, _, filenames in os.walk(path):
         for filename in filenames:
@@ -24,7 +32,7 @@ def build_model(path):
             cur_data.extend(freq_peaks)
             data.append(cur_data)
 
-    # Column names for DataFrame
+    # Column Names for DataFrame:
     cols = ["Chord Type", "File Name", "Min Harmonic", "Max Harmonic", "# of Harmonics"]
 
     for i in range(max_harm_length):
@@ -46,39 +54,40 @@ def build_model(path):
 
     print(df)
 
-    # Preprocessing data
+    # Preprocessing Data
     df["Chord Type"] = df["Chord Type"].replace("Major", 1)
     df["Chord Type"] = df["Chord Type"].replace("Minor", 0)
+    # df.to_csv('Training.csv', index=False)
 
-    # Columns used for training
     columns = ["Interval 1", "Interval 2", "Interval 3", "Interval 4"]
     columns.extend(["Interval 4_1", "Interval 5_1", "Interval 6_1"])
-
-    # Splitting the data
     train_X, val_X, train_y, val_y = train_test_split(df[columns], df["Chord Type"], test_size=0.40, random_state=0)
-
-    print(f"Total samples: {len(df)}")
-    print(f"Training samples: {len(train_X)}")
-    print(f"Validation samples: {len(val_X)}")
+    train_X.head()
+    training = train_X.copy()
+    training['Target'] = train_y
+    training.to_csv('Training.csv', index=False)
 
     # Model Building
     rfc = RandomForestClassifier(random_state=0)
     score_rfc = cross_val_score(rfc, train_X, train_y, cv=10).mean()
     print("Cross Val Score for Random Forest Classifier: {:.2f}".format(score_rfc))
-
-    # Defining and training classifier
+    # defining my classifier
     classifier = RandomForestClassifier(random_state=0)
-    classifier.fit(train_X, train_y)  # Training classifier
-    pred_y = classifier.predict(val_X)  # Making prediction on validation
+
+    classifier.fit(train_X, train_y)  # training classifier
+    pred_y = classifier.predict(val_X)  # making prediction on validation
     cm = confusion_matrix(val_y, pred_y)
     acc = accuracy_score(val_y, pred_y)
 
     print("Confusion Matrix:")
     print(cm)
     print("Accuracy Score: {:.2f}".format(acc))
+    rfc.fit(train_X, train_y)
 
     # Save the trained model to a file
+    import joblib
     joblib.dump(rfc, 'rfc_model.pkl')
+
 
 if __name__ == "__main__":
     build_model(r"C:\Users\User\Desktop\פרויקט בינה מלאכותית 20.5.24\Audio_Files")
